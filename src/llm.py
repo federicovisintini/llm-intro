5ab11e2ce9b8cbe122a35d9498459e0035e8d1b7
Federico Visintini <federico.visintini@olivia-software.com>
2023-05-21 16:41:49 +0200

    Add serpapi

--- src/llm.py
+++ src/llm.py
@@ -2,15 +2,15 @@ import os
 
 from langchain.agents import initialize_agent, AgentType, load_tools
 
-from src.secret import OPENAI_API_KEY, WOLFRAM_ALPHA_APPID
+from src.secret import OPENAI_API_KEY, SERPAPI_API_KEY
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, ConversationChain
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-os.environ["WOLFRAM_ALPHA_APPID"] = WOLFRAM_ALPHA_APPID
+os.environ["SERPAPI_API_KEY"] = SERPAPI_API_KEY
 
-llm = OpenAI(temperature=0.7, model_name="text-davinci-003")
+llm = OpenAI(temperature=0, model_name="text-davinci-003")
 
 prompt = PromptTemplate(
     input_variables=["user_request"],
@@ -20,18 +20,19 @@ prompt = PromptTemplate(
              'Use /// /// only for user output.'
 )
 
-tools = load_tools(["wolfram-alpha"])
+tools = load_tools(["serpapi", "wikipedia", "llm-math"], llm=llm)
 
 # chain = LLMChain(llm=llm, prompt=prompt)
 # conversation = ConversationChain(llm=llm, verbose=True)
-agent = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 if __name__ == '__main__':
-    input = "What would you do if you were as powerful as a singularity?"
+    input = "What was the low temperature in SF yesterday in Celsius?" \
+            "Was percentile of temperature in the year was it?"
     # print(chain.run(input))
 
     # output = conversation.predict(input="Hi there!")
     # output = conversation.predict(input="I'm doing well! Just having a conversation with an AI.")
     # print(output)
 
-    agent.run(input)
+    print(agent.run(input))
