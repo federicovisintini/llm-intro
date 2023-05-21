5d355ac5c147a2bc247d64d91d06edb6cd8c1b55
Federico Visintini <federico.visintini@olivia-software.com>
2023-05-21 11:04:41 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, html, Output, Input
+
+app = Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+app.layout = dbc.Container([
+    html.Br(),
+    dbc.Row(
+        html.H1("Singularity chatbot"), id='title_row'
+    ),
+    html.Br(),
+    dbc.Row([
+        html.H5("Insert you input here:"),
+        dbc.Input(id="input", placeholder="Type something...", type="text"),
+    ],  id='input_row'),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H5("This is your output:"),
+            html.P(id="output_user"),
+        ], width=6, id="output_user_col"),
+        dbc.Col([
+            html.H5("These are the LLM thoughts:"),
+            html.P(id="internal_thoughts"),
+        ], id="internal_thoughts_col"),
+    ],  id='output_row'),
+])
+
+
+@app.callback(Output("output_user", "children"), [Input("input", "value")])
+def output_text(value):
+    return f"LLM output({value})"
+
+
+@app.callback(Output("internal_thoughts", "children"), [Input("input", "value")])
+def thoughts_text(value):
+    return f"LLM thoughts({value})"
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host='127.0.0.1')
