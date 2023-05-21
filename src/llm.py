a00e84f5579e10cfab5faa8a1ebc1e22965429b6
Federico Visintini <federico.visintini@olivia-software.com>
2023-05-21 17:54:33 +0200

    Agents

--- src/llm.py
+++ src/llm.py
@@ -24,15 +24,16 @@ tools = load_tools(["serpapi", "wikipedia", "llm-math"], llm=llm)
 
 # chain = LLMChain(llm=llm, prompt=prompt)
 # conversation = ConversationChain(llm=llm, verbose=True)
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, max_iterations=4, verbose=True)
 
 if __name__ == '__main__':
-    input = "What was the low temperature in SF yesterday in Celsius?" \
-            "Was percentile of temperature in the year was it?"
+    input = "If you had a team of three or four students good at programming in 2023, when the AI sector is booming, " \
+            "which ideas of startup would you pursue? Give us some ideas in detail and give us a response of some " \
+            "paragraphs."
+    print(agent.run(input))
+
     # print(chain.run(input))
 
     # output = conversation.predict(input="Hi there!")
     # output = conversation.predict(input="I'm doing well! Just having a conversation with an AI.")
     # print(output)
-
-    print(agent.run(input))
