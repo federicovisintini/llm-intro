0ef08caeb06657cb0d450f3283636c96ad570e94
Federico Visintini <federico.visintini@olivia-software.com>
2023-05-21 18:37:17 +0200

    Add indexes

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,8 @@
 import os
 
 from langchain.agents import initialize_agent, AgentType, load_tools
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
 
 from src.secret import OPENAI_API_KEY, SERPAPI_API_KEY
 from langchain.llms import OpenAI
@@ -20,20 +22,26 @@ prompt = PromptTemplate(
              'Use /// /// only for user output.'
 )
 
-tools = load_tools(["serpapi", "wikipedia", "llm-math"], llm=llm)
-
-# chain = LLMChain(llm=llm, prompt=prompt)
+chain = LLMChain(llm=llm, prompt=prompt)
 # conversation = ConversationChain(llm=llm, verbose=True)
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, max_iterations=4, verbose=True)
+
+# tools = load_tools(["serpapi", "wikipedia", "llm-math"], llm=llm)
+# agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, max_iterations=4, verbose=True)
+
 
 if __name__ == '__main__':
-    input = "If you had a team of three or four students good at programming in 2023, when the AI sector is booming, " \
-            "which ideas of startup would you pursue? Give us some ideas in detail and give us a response of some " \
-            "paragraphs."
-    print(agent.run(input))
+    loader = PyPDFLoader('../data/facebook_10k.pdf')
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    query = "What is the revenue for the FY 2022?"
+    print(index.query(query))
+
+    # input = "If you had a team of three or four students good at programming in 2023, when the AI sector is booming, " \
+    #         "which ideas of startup would you pursue? Give us some ideas in detail and give us a response of some " \
+    #         "paragraphs."
 
     # print(chain.run(input))
-
+    # print(agent.run(input))
     # output = conversation.predict(input="Hi there!")
     # output = conversation.predict(input="I'm doing well! Just having a conversation with an AI.")
     # print(output)
