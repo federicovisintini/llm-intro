16784c88dcb7cd28568703f890f4a5882386d502
Federico Visintini <federico.visintini@olivia-software.com>
2023-05-21 11:35:46 +0200

    LLm example

--- src/llm.py
+++ src/llm.py
@@ -3,3 +3,7 @@ from src.secret import OPENAI_API_KEY
 from langchain.llms import OpenAI
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+llm = OpenAI(temperature=0.7, model_name="text-davinci-003")
+
+print(llm("What would be a good company name for a company that makes colorful socks?"))
